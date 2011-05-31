2f1c9934473f07517aac8ef8af5bc9a2f905ec8f
Viktors Rotanovs <viktors.rotanovs@gmail.com>
2011-05-31 18:22:53 +0200

    indentation fix

--- test/online.test.js
+++ test/online.test.js
@@ -10,17 +10,17 @@ var redecard = require('../index.js')
 module.exports = 
   { 'test getAuthorized success': function() {
       var details = { env: 'test'
-                  , amount: 0.01
-                  , type: redecard.TYPES.FULL_PAYMENT
-                  , installments: 0
-                  , supplierId: '36483184'
-                  , orderId: 'ORDER001'
-                  , cardNumber: '4111111111111111'
-                  , cardCode: '123'
-                  , cardExpMonth: 1
-                  , cardExpYear: 2016
-                  , cardFullName: 'JOHN DOE'
-                  }
+                    , amount: 0.01
+                    , type: redecard.TYPES.FULL_PAYMENT
+                    , installments: 0
+                    , supplierId: '36483184'
+                    , orderId: 'ORDER001'
+                    , cardNumber: '4111111111111111'
+                    , cardCode: '123'
+                    , cardExpMonth: 1
+                    , cardExpYear: 2016
+                    , cardFullName: 'JOHN DOE'
+                    }
       redecard.getAuthorized(
         details
         , function(err, data) {
